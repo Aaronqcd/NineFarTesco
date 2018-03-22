c4171b7e2e9dd012cc7d0b060d53d5aa1543ce0a
DESKTOP-HRPCBU0\qcd <812159920@qq.com>
2018-03-22 11:09:02 +0800

    初始化项目

---
+++ ecshop/data/config.php
@@ -0,0 +1,37 @@
+<?php
+// database host
+$db_host   = "localhost:3306";
+
+// database name
+$db_name   = "tesco";
+
+// database username
+$db_user   = "root";
+
+// database password
+$db_pass   = "123456";
+
+// table prefix
+$prefix    = "nf_";
+
+$timezone    = "Asia/Shanghai";
+
+$cookie_path    = "/";
+
+$cookie_domain    = "";
+
+$session = "1440";
+
+define('EC_CHARSET','utf-8');
+
+define('ADMIN_PATH','admin');
+
+define('AUTH_KEY', 'this is a key');
+
+define('OLD_AUTH_KEY', '');
+
+define('API_TIME', '');
+
+define('STORE_KEY','67f6b71f943f69f73d54cc8cfeca7aeb');
+
+?>
