102767e5675a407179c1187d00f538a2c75c0f95
DESKTOP-HRPCBU0\qcd <812159920@qq.com>
2018-03-29 10:35:57 +0800

    去掉ecshop版权信息

--- ecshop/includes/cls_ecshop.php
+++ ecshop/includes/cls_ecshop.php
@@ -18,7 +18,7 @@ if (!defined('IN_ECS'))
     die('Hacking attempt');
 }
 
-define('APPNAME', 'ECSHOP');
+define('APPNAME', 'NineFarTesco');
 define('VERSION', 'v3.6.0');
 define('RELEASE', '20161031');
 
